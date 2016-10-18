fcec98a4a108d9acf0008843055d423ecc5a0b45
Hang Cui <heatch@163.com>
2016-10-18 15:22:35 +1000

    split the operation list to order list and return list

--- src/mdp/component/Action.java
+++ src/mdp/component/Action.java
@@ -20,7 +20,30 @@ public class Action {
     public static int maxStore;
 
     // the operation of an action
-    public List<Integer> operations;
+    public List<Integer> orderList;
+
+    public List<Integer> returnList;
+
+    public Action() {
+        orderList = new ArrayList<>();
+        returnList = new ArrayList<>();
+    }
+
+    public List<Integer> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<Integer> orderList) {
+        this.orderList = orderList;
+    }
+
+    public List<Integer> getReturnList() {
+        return returnList;
+    }
+
+    public void setReturnList(List<Integer> returnList) {
+        this.returnList = returnList;
+    }
 
     public State generateNewState(State oldState) {
         State newState = new State(oldState);
@@ -28,7 +51,8 @@ public class Action {
             List<Integer> items = oldState.getItems();
             List<Integer> newItems = new ArrayList<>();
             for(int i = 0; i < newState.getTypeOfItems(); ++i) {
-                newItems.add(items.get(i) + operations.get(i));
+                newItems.add(items.get(i) + orderList.get(i));
+                newItems.add(items.get(i) - returnList.get(i));
             }
             newState.setItems(newItems);
         }
@@ -42,20 +66,11 @@ public class Action {
         int numOfOrderItem = 0;
         //
         int numOfReturnItem = 0;
-        // whether the order is valid
-        boolean valid = true;
 
         // add all order operation together
-        for(Integer operation : operations) {
-            if (operation > 0) {
-                numOfOrderItem += operation;
-            }
-        }
-
-        for(Integer operation : operations) {
-            if(operation < 0) {
-                numOfReturnItem -= operation;
-            }
+        for(int i = 0; i < orderList.size(); ++i) {
+            numOfOrderItem += orderList.get(i);
+            numOfReturnItem += returnList.get(i);
         }
 
         return numOfOrderItem < maxOrder && numOfReturnItem < maxReturn;
